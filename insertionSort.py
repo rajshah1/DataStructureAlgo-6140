8c2639debe829e90976fa0b365d77164f93bc3f3
rajshah1 <raj.shah@infosys.com>
2021-02-28 03:32:55 +0530

    3. InsertionSort Remote func call

--- insertionSort.py
+++ insertionSort.py
@@ -7,7 +7,7 @@ def InsertSort(array):
             array[y+1]=array[y]
             y=y-1
         array[y+1]=key
-
+    print(array)
 #array=[2,1,6,9,10,11,60,14,15,22,8,88,40,24,36,75,888,14]
 #InsertSort(array)
 #print(array)
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from numpy import random as rand
 from insertionSort import InsertSort as insertionSortCall
+
 # Random Array Generator which Generates 1-D Array 
 # Passing The same array to the import function call 
 
-arrayInput=rand.randint(10000,size=1000);
+arrayInput=rand.randint(10000,size=1000)
+
+# Function Call to InsertionSort
+insertionSortCall(arrayInput)
