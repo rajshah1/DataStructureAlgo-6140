c9c6f767a6e0e51b0cdeefeab843b1f04dee2a86
rajshah1 <raj.shah@infosys.com>
2021-02-28 03:13:35 +0530

    1. Init with Rand and function call back to InsertionSort

---
+++ insertionSort.py
@@ -0,0 +1,13 @@
+def InsertSort(array):
+   
+    for x in range(1,len(array)):
+        key=array[x]
+        y=x-1
+        while y>=0 and array[y]>key:
+            array[y+1]=array[y]
+            y=y-1
+        array[y+1]=key
+
+#array=[2,1,6,9,10,11,60,14,15,22,8,88,40,24,36,75,888,14]
+#InsertSort(array)
+#print(array)
---
+++ main.py
@@ -0,0 +1,6 @@
+from numpy import random as rand
+from insertionSort import InsertSort as insertionSortCall
+# Random Array Generator which Generates 1-D Array 
+# Passing The same array to the import function call 
+
+arrayInput=rand.randint(10000,size=1000);
