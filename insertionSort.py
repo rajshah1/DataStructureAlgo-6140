3bb89e4e19d8e2b8658965ef414c3505ad6538f5
rajshah1 <raj.shah@infosys.com>
2021-02-28 03:41:01 +0530

    4. Timing for Performance

--- insertionSort.py
+++ insertionSort.py
@@ -1,4 +1,4 @@
-def InsertSort(array):
+def insertionSort(array):
    
     for x in range(1,len(array)):
         key=array[x]
@@ -8,6 +8,10 @@ def InsertSort(array):
             y=y-1
         array[y+1]=key
     print(array)
+
+
+# Use for Indiviual Analysis and Code Correctness
+
 #array=[2,1,6,9,10,11,60,14,15,22,8,88,40,24,36,75,888,14]
 #InsertSort(array)
 #print(array)
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from numpy import random as rand
-from insertionSort import InsertSort as insertionSortCall
-
+from insertionSort import insertionSort as insertionSortCall
+import time as t
 # Random Array Generator which Generates 1-D Array 
 # Passing The same array to the import function call 
 
 arrayInput=rand.randint(10000,size=1000)
 
 # Function Call to InsertionSort
+insertionSort_Start=t.time()
 insertionSortCall(arrayInput)
+insertionSort_End=t.time()
+
+print(insertionSort_End-insertionSort_Start)
